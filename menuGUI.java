bb14ad4e1db55e9d1a51fb6336ece4944b1c3f55
a1426 <66330789+a1426@users.noreply.github.com>
2023-05-15 16:46:41 -0500

    Update menuGUI.java to account for the new listener

--- menuGUI.java
+++ menuGUI.java
@@ -7,7 +7,8 @@ public class menuGUI extends JFrame{
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JMenu mainMenu=new JMenu("Actions");
         JMenuItem custom = new JMenuItem("Create a custom Turing Machine");
-        custom.addActionListener(new EndListener());
+        CreateListener cl= new CreateListener(menu);
+        custom.addActionListener(cl);
         mainMenu.add(custom);
         JMenu preset = new JMenu("Select a preset Turing Machine");
         preset.addActionListener(new EndListener());
@@ -15,13 +16,12 @@ public class menuGUI extends JFrame{
         JMenuItem exit = new JMenuItem("Quit");
         exit.addActionListener(new EndListener());
         mainMenu.add(exit);
-        JLabel welcome = new JLabel("To get started, use the menu in the upper left hand corner.");
+        JLabel welcome = new JLabel("Start with the menu in the upper left hand corner.");
         menu.add(welcome);
         JMenuBar mBar = new JMenuBar();
         mBar.add(mainMenu);
         menu.setJMenuBar(mBar);
         menu.setVisible(true);
-
     }
     public static void main(String[] args){
         new menuGUI();
