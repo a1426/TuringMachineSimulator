d31bdf4093a461499f71bf77cfc04f5d89bfbd5d
a1426 <66330789+a1426@users.noreply.github.com>
2023-05-26 10:01:12 -0500

    Final comments

--- TuringMachine.java
+++ TuringMachine.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
@@ -6,9 +5,8 @@ import java.util.Map;
 public class TuringMachine {
     //The list of all internal states.
     private List<Integer> stateList;
-    //Takes in a mapping of the object state, and the tape state, seperated by a comma. The format is "[state,tape,left or right]", where left = -1, right=1.
+    //The key is the object state, and the tape state, separated by a comma. The format is "state,tape,left or right", where left = -1, right=1.
     private Map<String,String> function;
-    //The list of states that cause the machine to halt.
     //The tape object.
     private Tape tape;
     private int initial;
@@ -16,18 +14,8 @@ public class TuringMachine {
     public Tape getTape(){
         return tape;
     }
-    public int getState(){
-        return initial;
-    }
     public int getIndex(){
         return index;
-    }
-    public int maxState(){
-        int a=stateList.get(0);
-        for(int i=1; i<stateList.size();i++){
-            a=Math.max(a,stateList.get(i));
-        }
-        return a;
     }
     public TuringMachine(List<Integer> sl, HashMap<String,String> f, int blank, int i){
         stateList=sl;
@@ -39,17 +27,13 @@ public class TuringMachine {
     public boolean run(){
         //While the state isn't a halt state, it uses the map to determine the new state, written value, and direction.
         String[] map=function.get(initial+","+tape.getElement(index)).split(",");
-
+        //Writes to the tape, moves, and sees the new state/whether it halted.
         tape.setElement(index,Integer.parseInt(map[1]));
-        System.out.println("Moved"+Integer.parseInt(map[2]));
         index+=Integer.parseInt(map[2]);
         if(map[0].equals("H")){
             return true;
         }
         else initial=Integer.parseInt(map[0]);
-        System.out.println("State:"+Integer.parseInt(map[0]));
         return false;
-
-
     }
 }
