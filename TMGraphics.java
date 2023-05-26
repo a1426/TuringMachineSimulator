e8088b3210c6bf523fb29a583816dcf52dd60a13
a1426 <66330789+a1426@users.noreply.github.com>
2023-05-26 10:12:09 -0500

    Final Comments

--- TMGraphics.java
+++ TMGraphics.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.util.Arrays;
 
 public class TMGraphics extends Canvas {
     private TuringMachine tm;
@@ -7,11 +6,10 @@ public class TMGraphics extends Canvas {
         this.tm=tm;
     }
     public void paint(Graphics g) {
-        int index= tm.getIndex();
-        Tape tape = tm.getTape();
-        System.out.println(index);
+        //Draws the head and Squares
         setBackground(Color.WHITE);
         setForeground(Color.BLACK);
+        g.setColor(Color.BLACK);
         g.drawRect(0,100,1311,100);
         for(int i=0; i<=22; i++){
             g.drawLine(57*i,100,57*i,200);
@@ -19,13 +17,12 @@ public class TMGraphics extends Canvas {
         int s=10;
         int[] xT={(1311-s)/2,1311/2,(1311+s)/2};
         int[] yT={50,60,50};
-        g.setColor(Color.BLACK);
         g.fillPolygon(xT,yT,3);
-        int[] c = tape.content(index);
+        //Fills in the content of the TM in each square.
+        int[] c = tm.getTape().content(tm.getIndex());
         for(int i=0; i<23; i++){
             g.drawString(c[i]+"",57*i,155);
         }
-
         }
 }
 
